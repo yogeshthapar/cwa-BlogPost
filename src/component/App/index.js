7340ecd5ade4c1338d539f2b376cf685538c9d22
yogesh.goyal <yogesh.goyal1@wipro.com>
2021-01-20 22:07:32 +0000

    BT:(Feat - Added Footer)

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
+import Footer from '../Footer';
 
 /**
  * App component.
@@ -10,7 +11,7 @@ const App = () => (
   <div className="bt--container-page">
     <Header />
     Main
-    Footer
+    <Footer />
   </div>
 );
 
---
+++ src/component/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Footer.css';
+/**
+ * Footer component.
+ */
+const Footer = () => (
+  <footer className="bt--wrapper-footer" />
+);
+
+export default Footer;
